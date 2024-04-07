b2d287d2f3330cfd67d1c6b62bcac7b221ea27d4
preity-singh <preitysingh023@gmail.com>
2024-04-06 18:05:52 -0700

    Toggle the state

--- client/src/app/components/ToggleView.js
+++ client/src/app/components/ToggleView.js
@@ -1,7 +1,18 @@
-const ToggleView = ({ text, onClick }) => {
+import React, { useState } from 'react';
+
+const ToggleView = ({ onClick }) => {
+    const [isCardView, setIsCardView] = useState(true);
+
+    const handleClick = () => {
+        setIsCardView(prevState => !prevState); // Toggle the state
+        if (onClick) {
+            onClick();
+        }
+    };
+
     return ( 
         <main>
-            <button onClick={onClick}>Hello</button>
+            <button onClick={handleClick}>{isCardView ? 'Card View' : 'Map View'}</button>
         </main>
     );
 }

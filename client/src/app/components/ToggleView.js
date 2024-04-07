2b509f9ec4c29e66e488cbedc7d9454738bd3420
preity-singh <preitysingh023@gmail.com>
2024-04-06 20:12:48 -0700

    style

--- client/src/app/components/ToggleView.js
+++ client/src/app/components/ToggleView.js
@@ -12,7 +12,17 @@ const ToggleView = ({ onClick }) => {
 
     return ( 
         <main>
-            <button className="button" onClick={handleClick}>{isCardView ? 'Card View' : 'Map View'}</button>          
+            {/* <button className="button" onClick={handleClick}>{isCardView ? 'Card View' : 'Map View'}</button> */}
+
+            <div className = "toggle">
+                <input type="radio" name="toggle" value="Card View" className="card-view" id="card-view" />
+                <label for="card-view">Card View</label>
+
+                <input type="radio" name="toggle" value="Map View" className="map-view" id="map-view" />
+                <label for="map-view">Map View</label>
+
+                <div className="selector">&nbsp;</div> {/*i dont think this is working*/}
+            </div>          
         </main>
     );
 }

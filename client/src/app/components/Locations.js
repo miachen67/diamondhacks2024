9c782af33a916b64f7168ec91db22fad973b50f7
miachen67 <miachen2005@gmail.com>
2024-04-06 18:26:09 -0700

    styled locationCards

--- client/src/app/components/Locations.js
+++ client/src/app/components/Locations.js
@@ -1,17 +1,27 @@
 import Image from "next/image";
 import img from '../../../public/next.svg'
 
-const Locations = ({img, locationName, rating}) => {
+const Locations = ({img, locationName, rating, specialFeatures}) => {
     return (  
         <div className="locationCard">
-            <Image 
-            src = {img} 
-            alt={"Image of Location next"} 
-            width = {300}
-            height = {100}
-            />
-            <h3>{locationName}</h3>
-            <h5>{rating}</h5>
+            <div className="imageCard">
+                <Image 
+                src = {img} 
+                alt={"Image of Location next"} 
+                width = {300}
+                height = {100}
+                className="imageCard"
+                />
+            </div>
+            <div className="locationName">
+                <p>{locationName}</p>
+            </div>
+            <div className="rating">
+                <p>{rating}</p>
+            </div>
+            <div className="specialFeatures">
+                <p>{specialFeatures}</p>
+            </div>
         </div>
     );
 }

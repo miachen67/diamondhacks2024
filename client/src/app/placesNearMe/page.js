3acba0e20d10acbfd0dd698ec0faed657a395c4d
preity-singh <preitysingh023@gmail.com>
2024-04-06 17:44:51 -0700

    initial preity commit

--- client/src/app/placesNearMe/page.js
+++ client/src/app/placesNearMe/page.js
@@ -1,7 +1,11 @@
+'use client';
+
+import ToggleView from "../components/ToggleView";
+
 const PlacesNearMe = () => {
     return (  
         <main>
-            
+            <ToggleView onClick={() => console.log("Button clicked!")} />
         </main>
     );
 }
